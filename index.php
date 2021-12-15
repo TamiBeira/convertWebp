80226d18155093b9d859aa1b2b0020588972a77d
Tamiris Beira <tamirisbeira@gmail.com>
2021-12-15 03:12:24 -0300

    Add extensões e corrigindo bugs

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/*Analisa se os arquivos que estão dentro da pastas são imagens*/
+$pasta = "img";
+$imagens = scandir($pasta);
+
+$extensoes =['.png', '.jpg', '.jpeg'];
+$webp = '.webp';
+
+/*Tirar os pontos que não são relacionados as imagens*/
+$ponto = array_shift($imagens);
+$ponto = array_shift($imagens);
+
+/*Imprime o Array de imagens*/
+//print_r($imagens);
+
+foreach($imagens as $imagem){
+
+    /*Remove as 4 ultimas strings do arquivo*/
+    $imagem = substr($imagem, 0,-4);
+
+    /*Add extensões*/
+    $entradaPNG = '/var/www/lynk/convertPHP/img/'.$imagem.$extensoes[0];
+    $entradaJPG = '/var/www/lynk/convertPHP/img/'.$imagem.$extensoes[1];
+    $entradaJPEG = '/var/www/lynk/convertPHP/img/'.$imagem.$extensoes[2];
+
+    $saida = '/var/www/lynk/convertPHP/img_convert/'.$imagem.$webp;
+
+    /*Executa a ação*/
+    exec("/var/www/lynk/convertPHP/libwebp/examples/cwebp {$entradaPNG} -o {$saida}");
+    exec("/var/www/lynk/convertPHP/libwebp/examples/cwebp {$entradaJPG} -o {$saida}");
+    exec("/var/www/lynk/convertPHP/libwebp/examples/cwebp {$entradaJPEG} -o {$saida}");
+
+    /*Imprime onde os arquivos foram salvos*/
+    echo "<p>{$saida}</p>";
+}
+
+?>
